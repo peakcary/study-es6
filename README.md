68c68596686b32dbb8bd5cd791ab13803ca0c2de
peak <peak@peakdeMacBook-Pro.local>
2016-08-31 17:24:13 +0800

    add webpack

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # study-es6
 
 babel es6.js -w -o es5.js
+
+[参考](https://github.com/rauschma/webpack-es6-demo)
---
+++ es6/main.js
@@ -0,0 +1,3 @@
+import Point from './Point.js';
+var body = document.querySelector('body');
+body.textContent = 'Good pint' + new Point(11,23);
